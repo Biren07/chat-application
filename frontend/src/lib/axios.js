99e81a1c801317a2cd22f5c733ea53efeb648572
[firstname lastname] <dhamib610@gmail.com>
2025-09-26 22:10:17 +0545

    update the axios.js  file

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chat-application-2-j309.onrender.com/api" : "/api",
-  withCredentials: true,
+  baseURL: import.meta.env.MODE === "development"
+    ? "https://chat-application-2-j309.onrender.com/api"
+    : "/api",
+  withCredentials: true, 
 });
