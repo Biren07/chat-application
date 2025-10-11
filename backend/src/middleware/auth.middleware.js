8442c0a9b3651052ea48ecbb4153716b5e4a5cc2
[firstname lastname] <dhamib610@gmail.com>
2025-10-11 23:24:49 +0545

    update the file

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -4,25 +4,11 @@ import { ENV } from "../lib/env.js";
 
 export const protectRoute = async (req, res, next) => {
   try {
-    let token;
-
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer ")
-    ) {
-      token = req.headers.authorization.split(" ")[1];
-    } else if (req.cookies.jwt) {
-      token = req.cookies.jwt;
-    }
-
-    if (!token)
-      return res
-        .status(401)
-        .json({ message: "Unauthorized - No token provided" });
+    const token = req.cookies.jwt;
+    if (!token) return res.status(401).json({ message: "Unauthorized - No token provided" });
 
     const decoded = jwt.verify(token, ENV.JWT_SECRET);
-    if (!decoded)
-      return res.status(401).json({ message: "Unauthorized - Invalid token" });
+    if (!decoded) return res.status(401).json({ message: "Unauthorized - Invalid token" });
 
     const user = await User.findById(decoded.userId).select("-password");
     if (!user) return res.status(404).json({ message: "User not found" });
